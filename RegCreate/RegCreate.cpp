97ba0c58349334ed4d63e335d9af0819c65b1176
Muhammet Karul <64870233+Karuulme@users.noreply.github.com>
2022-10-26 21:31:32 +0300

    Value and Key Added

--- RegCreate/RegCreate.cpp
+++ RegCreate/RegCreate.cpp
@@ -1,40 +1,19 @@
-﻿
-#include <iostream>
-//#include <winreg.h>
+﻿#include <iostream>
 #include <windows.h>
 using namespace std;
 int main()
 {
-	//LSTATUS RegCreateKeyExA(
-	//	[in]            HKEY                        hKey,       				HKEY_USERS      
-	//	[in]            LPCSTR                      lpSubKey,					DnemeUygulamasÄ±      
-	//	DWORD                       Reserved,									0      
-	//	[in, optional]  LPSTR                       lpClass,					NULL      
-	//	[in]            DWORD                       dwOptions,					REG_OPTION_BACKUP_RESTORE     
-	//	[in]            REGSAM                      samDesired,					KEY_SET_VALUE      
-	//	[in, optional]  const LPSECURITY_ATTRIBUTES lpSecurityAttributes,		lsRegOpen      
-	//	[out]           PHKEY                       phkResult,					      
-	//	[out, optional] LPDWORD                     lpdwDisposition				      
-	//);
 	HKEY hkRegOpen;
 	DWORD dRegReturn;
 
-	LONG res = RegCreateKeyExA(HKEY_LOCAL_MACHINE, "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA", 0, NULL, REG_OPTION_VOLATILE, KEY_ALL_ACCESS, NULL, &hkRegOpen, &dRegReturn);
+	LONG res = RegCreateKeyExA(HKEY_CURRENT_USER, "deneme\\AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA", 0, NULL, REG_OPTION_NON_VOLATILE, KEY_SET_VALUE, NULL, & hkRegOpen, & dRegReturn);
+	
+	LPCSTR value = "memet";
 
-	if (res == REG_CREATED_NEW_KEY) {
-		cout << "Başarılı" << endl;
-
-	}
-	//	CloseHandle(res);
-
-		/*const char* keyloc = "wulf\\comms\\memetbirdeneme";
-
-		LONG res = RegCreateKeyExA(HKEY_CURRENT_USER, keyloc, 0, NULL, REG_OPTION_NON_VOLATILE, KEY_ALL_ACCESS,
-			NULL, &hkRegOpen, &dRegReturn);
-		if (res == ERROR_SUCCESS) {
-			return 0;
-		}
-		else {
-			return -1;
-		}*/
+	LPCTSTR  data = L"Teneme\0";
+	RegSetValueExA(hkRegOpen,value,0, REG_SZ, (LPBYTE)(char)data, strlen((char*)data)+1);
+	
+	
+	
+	RegCloseKey(hkRegOpen);
 }
