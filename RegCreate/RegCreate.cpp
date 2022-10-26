79ff2c3bd27a4940ea41b02b08ffb9fa41a46dbd
Muhammet Karul <64870233+Karuulme@users.noreply.github.com>
2022-10-26 21:38:35 +0300

    HKEY_CURRENT_USER  Add Key and Value successful

--- RegCreate/RegCreate.cpp
+++ RegCreate/RegCreate.cpp
@@ -5,14 +5,13 @@ int main()
 {
 	HKEY hkRegOpen;
 	DWORD dRegReturn;
-
+	
 	LONG res = RegCreateKeyExA(HKEY_CURRENT_USER, "deneme\\AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA", 0, NULL, REG_OPTION_NON_VOLATILE, KEY_SET_VALUE, NULL, & hkRegOpen, & dRegReturn);
 	
 	LPCSTR value = "memet";
 
-	LPCTSTR  data = L"Teneme\0";
-	RegSetValueExA(hkRegOpen,value,0, REG_SZ, (LPBYTE)(char)data, strlen((char*)data)+1);
-	
+	LPCSTR  data = "Teneme\0";
+	RegSetValueExA(hkRegOpen,value,0, REG_SZ, (LPBYTE)data, strlen((char*)data)+1);
 	
 	
 	RegCloseKey(hkRegOpen);
